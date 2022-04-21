07a053f28e8356938bafb8da77ae2a976b1a9b02
mani <100167532+Mani8217@users.noreply.github.com>
2022-04-21 06:52:20 +0200

    some ubdates

--- mani-shidfar/semana5/startup-matchmaker/starter-code/scripts/main.js
+++ mani-shidfar/semana5/startup-matchmaker/starter-code/scripts/main.js
@@ -5,7 +5,7 @@
 // Execution
 
 function toggleMenu() {
-  var menuBox = document.getElementById("list");
+  let menuBox = document.getElementById("list");
   if (menuBox.style.display == "block") {
     menuBox.style.display = "none";
   } else {
