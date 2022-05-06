1e56cf644a24416b66b27df08864ee7c0f79c032
Al2com <acomenge@iesfuentesanluis.org>
2022-05-06 14:02:33 +0200

    Tarea_calculadora_terminada

--- alvaro-comenge/semana08/javascript-calculator/starter-code/scripts/app.js
+++ alvaro-comenge/semana08/javascript-calculator/starter-code/scripts/app.js
@@ -6,29 +6,37 @@
 const x = parseFloat(prompt('dime un numero'));
 const z =( prompt('dime la operacion'));
 const y = parseFloat(prompt('dime el otro numero'));
-
-// const suma = x + y;
-// const resta = x - y;
-// const multiplicacion = x * y;
-// const division = x / y;  
-
-if (z == '+') {
-  const suma = x + y;
-  alert(suma)
-} else if (z == '-') { 
-  const resta = x - y;
-  alert(resta)
-} else if (z == '*') {
-  const multiplicacion = x * y;
-  alert(multiplicacion)
-} else if (z == '/') {
-  const division = x / y;
-  alert(division)
-}
-
-
+// SUMA NORMAL
 // SUMA NORMAL
 // const x = parseFloat(prompt("dime un numero"));
 // const y = parseFloat(prompt("dime el otro numero"))
 // const suma = x + y;
 // alert(suma);
+
+// CALCULADORA-BÁSICA
+
+if (z == "+") {
+  const suma = x + y;
+  alert(suma);
+} else if (z == "-") {
+  const resta = x - y;
+  alert(resta);
+} else if (z == "*") {
+  const multiplicacion = x * y;
+  alert(multiplicacion);
+} else if (z == "/") {
+  const division = x / y;
+  alert(division);
+
+  // CALCULADORA AVANZADA
+} else if (z == "**") {
+  const potencia = x ** y;
+  alert(potencia);
+} else if (z == "r") {
+  const raiz =Math.sqrt(x);
+  alert(raiz);
+}
+
+
+
+
