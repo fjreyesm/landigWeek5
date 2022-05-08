08edcd52fc437c5d79e7fa416af730296abc46c6
fjreyesm <franklin.reyes.2003@gmail.com>
2022-05-08 11:16:00 +0200

    mejoras y calc avanzada

--- franklin-reyes/semana08/scripts/app.js
+++ franklin-reyes/semana08/scripts/app.js
@@ -5,42 +5,42 @@
 // * Escribe tu código debajo.
 let calcular = true;
 let tipo = 1;
+1;
 let i = 0;
 do {
   let tipo = prompt("Desea realizar una operacion basica(1) o Avanzazada(2)");
   if (tipo == 1) {
-    let operacion = prompt("Que operacion deseas realizar? ( + - * /)");
+    let operacion1 = prompt("Que operacion deseas realizar? ( + - * /)");
+    let numero1 = parseInt(prompt("Introduce el primer operando"));
+    let numero2 = parseInt(prompt("Introduce el segundo operando"));
+
+    if (operacion1 == "+") {
+      alert("El resultado de la suma es: " + (numero1 + numero2));
+    } else if (operacion1 == "-") {
+      alert("El resultado de la resta es: " + (numero1 - numero2));
+    } else if (operacion1 == "*") {
+      alert("El resultado de la multiplicación es: " + numero1 * numero2);
+    } else if (operacion1 == "/") {
+      alert("El resultado de la división es: " + numero1 / numero2);
+    } else {
+      alert("Operación no válida");
+    }
   } else if (tipo == 2) {
-    let operacion = prompt(
-      "Que operacion deseas realizar?  Raiz Cuadrada(1) o Potencia(2)"
+    let operacion2 = prompt(
+      "Que operacion deseas realizar? Potencia(1) o Raiz Cuadrada(2)"
     );
+    if (operacion2 == 1) {
+      let numero1 = parseInt(prompt("Introduce el primer operando"));
+      let numero2 = parseInt(prompt("Introduce el segundo operando"));
+      if (operacion2 == "1") {
+        alert("El resultado de la resta es: " + Math.pow(numero1, numero2));
+      }
+    } else if (operacion2 == 2) {
+      let numero1 = parseInt(prompt("A cual numero quieres calcular la raiz2"));
+      alert("El resultado de la raiz cuadrada es: " + Math.sqrt(numero1));
+    }
   }
 
-  let numero1 = parseInt(prompt("Introduce el primer operando"));
-  let numero2 = parseInt(prompt("Introduce el segundo operando"));
-  if (
-    (operacion === "+") |
-      (operacion === "-") |
-      (operacion === "*") |
-      (operacion === "/") |
-      (operacion === "2") &&
-    typeof numero1 === "number" &&
-    typeof numero2 === "number"
-  ) {
-    if (operacion == "+") {
-      alert("El resultado de la suma es: " + (numero1 + numero2));
-    } else if (operacion == "-") {
-      alert("El resultado de la resta es: " + (numero1 - numero2));
-    } else if (operacion == "*") {
-      alert("El resultado de la multiplicación es: " + numero1 * numero2);
-    } else if (operacion == "/") {
-      alert("El resultado de la división es: " + numero1 / numero2);
-    } else if (operacion === "2") {
-      alert("El resultado de la potencia es: " + Math.pow(numero1, numero2));
-    }
-  } else {
-    alert("Operación no válida o alguno de los operandos no es un número");
-  }
   i++;
   let resp = prompt(" deseas hacer otra operacion? ( si/no)");
   if (resp == "no" || resp == "No" || resp == "NO") {
